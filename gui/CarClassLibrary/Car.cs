f0f0e7171f8b18adc613078ccd517b5e6c820ca5
Roger <88466461+RogerRandomDev@users.noreply.github.com>
2024-05-01 21:30:44 -0700

    Add files via upload

---
+++ gui/CarClassLibrary/Car.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarClassLibrary
+{
+    public class Car
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public decimal Price { get; set; }
+
+        public string Color { get; set; }
+        public int Year { get; set; }
+
+        public Car(string make,string model,decimal price,string color,int year)
+        {
+            this.Make = make;
+            this.Model = model;
+            this.Price = price;
+            this.Color = color;
+            this.Year = year;
+        }
+        public Car()
+        {
+            this.Make = "Nothing yet";
+            this.Model = "Nothing yet";
+            this.Price = 0;
+            this.Color = "Unknown";
+            this.Year = 0000;
+        }
+
+        public string getMake()
+        {
+            return this.Make;
+        }
+
+        public void setMake(string newName)
+        {
+            this.Make = newName;
+        }
+
+
+        public string Display
+        {
+            get
+            {
+                return String.Format("{0} {1} ${2} {3} {4}", Make, Model, Price,Color,Year);
+            }
+        }
+    }
+}
