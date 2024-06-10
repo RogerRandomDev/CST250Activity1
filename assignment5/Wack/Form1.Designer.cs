f830568085ca76afc782b0e324b12a52c33d2831
Roger <88466461+RogerRandomDev@users.noreply.github.com>
2024-06-09 21:22:10 -0700

    added week 5

---
+++ assignment5/Wack/Form1.Designer.cs
@@ -0,0 +1,311 @@
+﻿namespace CST_250_Activity_5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button4 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Industry-Bold", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(221)))), ((int)(((byte)(230)))), ((int)(((byte)(237)))));
+            this.label1.Location = new System.Drawing.Point(47, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(11, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = ".";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(82)))), ((int)(((byte)(109)))), ((int)(((byte)(130)))));
+            this.button1.Font = new System.Drawing.Font("Industry-Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(221)))), ((int)(((byte)(230)))), ((int)(((byte)(237)))));
+            this.button1.Location = new System.Drawing.Point(12, 375);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(80, 50);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "START";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(82)))), ((int)(((byte)(109)))), ((int)(((byte)(130)))));
+            this.button2.Font = new System.Drawing.Font("Industry-Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(221)))), ((int)(((byte)(230)))), ((int)(((byte)(237)))));
+            this.button2.Location = new System.Drawing.Point(98, 375);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(80, 50);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "STOP";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(82)))), ((int)(((byte)(109)))), ((int)(((byte)(130)))));
+            this.button3.Font = new System.Drawing.Font("Industry-Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(221)))), ((int)(((byte)(230)))), ((int)(((byte)(237)))));
+            this.button3.Location = new System.Drawing.Point(184, 375);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(80, 50);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "RESET";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Red;
+            this.button4.Location = new System.Drawing.Point(111, 168);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(50, 50);
+            this.button4.TabIndex = 4;
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Industry-Bold", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(221)))), ((int)(((byte)(230)))), ((int)(((byte)(237)))));
+            this.label2.Location = new System.Drawing.Point(209, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 17);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Score: 0";
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.button5.Location = new System.Drawing.Point(111, 112);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(50, 50);
+            this.button5.TabIndex = 6;
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.button6.Location = new System.Drawing.Point(111, 224);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(50, 50);
+            this.button6.TabIndex = 7;
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Industry-Bold", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(221)))), ((int)(((byte)(230)))), ((int)(((byte)(237)))));
+            this.label3.Location = new System.Drawing.Point(168, 428);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(75, 17);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "High Score:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Industry-Bold", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(221)))), ((int)(((byte)(230)))), ((int)(((byte)(237)))));
+            this.label4.Location = new System.Drawing.Point(55, 182);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(165, 36);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "GAME OVER";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Industry-Bold", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(221)))), ((int)(((byte)(230)))), ((int)(((byte)(237)))));
+            this.label5.Location = new System.Drawing.Point(9, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(39, 17);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Time:";
+            // 
+            // button7
+            // 
+            this.button7.BackColor = System.Drawing.Color.Yellow;
+            this.button7.Location = new System.Drawing.Point(111, 56);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(50, 50);
+            this.button7.TabIndex = 11;
+            this.button7.UseVisualStyleBackColor = false;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.BackColor = System.Drawing.Color.Maroon;
+            this.button8.Location = new System.Drawing.Point(111, 280);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(50, 50);
+            this.button8.TabIndex = 12;
+            this.button8.UseVisualStyleBackColor = false;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.BackColor = System.Drawing.Color.Red;
+            this.button9.Enabled = false;
+            this.button9.Location = new System.Drawing.Point(-2, 112);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(50, 50);
+            this.button9.TabIndex = 13;
+            this.button9.UseVisualStyleBackColor = false;
+            // 
+            // button10
+            // 
+            this.button10.BackColor = System.Drawing.Color.Yellow;
+            this.button10.Enabled = false;
+            this.button10.Location = new System.Drawing.Point(-2, 168);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(50, 50);
+            this.button10.TabIndex = 14;
+            this.button10.UseVisualStyleBackColor = false;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Industry-Bold", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(221)))), ((int)(((byte)(230)))), ((int)(((byte)(237)))));
+            this.label6.Location = new System.Drawing.Point(58, 128);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(213, 17);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "Click these squares to earn points.";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Industry-Bold", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(221)))), ((int)(((byte)(230)))), ((int)(((byte)(237)))));
+            this.label7.Location = new System.Drawing.Point(58, 184);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(214, 17);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "Click these squares for extra time.";
+            // 
+            // label8
+            // 
+            this.label8.Font = new System.Drawing.Font("Industry-Bold", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(221)))), ((int)(((byte)(230)))), ((int)(((byte)(237)))));
+            this.label8.Location = new System.Drawing.Point(29, 221);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(214, 41);
+            this.label8.TabIndex = 17;
+            this.label8.Text = "Clicking anywhere else will penalize your time!";
+            this.label8.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(39)))), ((int)(((byte)(55)))), ((int)(((byte)(77)))));
+            this.ClientSize = new System.Drawing.Size(278, 446);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Whack-A-Mole";
+            this.Click += new System.EventHandler(this.Form1_Click);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ assignment5/Wack/Form1.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace CST_250_Activity_5
+{
+    public partial class Form1 : Form
+    {
+
+        public static Stopwatch watch = new Stopwatch();
+        private Random random = new Random();
+        public int score = 0; // score tracker
+        public int highscore = 0;
+        private int xMin = 20; // target minimum x boundary
+        private int yMin = 50; // target minimum y boundary
+        private int xMax = 250; // target max x boundary
+        private int yMax = 350; // target max y boundary
+        private TimeSpan additionalTime = TimeSpan.Zero;
+
+        public Form1()
+        {
+            InitializeComponent();
+            timer1.Enabled = false;
+            button4.Visible = false;
+            button5.Visible = false;
+            button6.Visible = false;
+            button7.Visible = false;
+            button8.Visible = false;
+            label4.Visible = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            watch.Start();
+            timer1.Enabled = true;
+            label2.Text = "Score: 0";
+            label4.Visible = false;
+            button9.Visible = false;
+            button10.Visible = false;
+            label6.Visible = false;
+            label7.Visible = false;
+            label8.Visible = false;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            watch.Stop();
+            timer1.Enabled = false;
+            button4.Visible = false;
+            button5.Visible = false;
+            button6.Visible = false;
+            button7.Visible = false;
+            button8.Visible = false;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            watch.Reset();
+            score = 0;
+            timer1.Enabled = false;
+            button4.Visible = false;
+            button5.Visible = false;
+            button6.Visible = false;
+            button7.Visible = false;
+            button8.Visible = false;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            TimeSpan elapsed = watch.Elapsed + additionalTime;
+
+            // Format the elapsed time to a readable format
+            string formattedTime = string.Format(
+                "{2:00}",
+                (int)elapsed.TotalHours,
+                elapsed.Minutes,
+                elapsed.Seconds,
+                elapsed.Milliseconds / 10);
+
+            label1.Text = formattedTime;
+
+            // Main target
+            if (random.Next(0, 10) < 8)
+            {
+                int maxWidth = xMax - xMin - button4.Width;
+                int maxHeight = yMax - yMin - button4.Height;
+
+                button4.Left = random.Next(xMin, xMax - button4.Width);
+                button4.Top = random.Next(yMin, yMax - button4.Height);
+                button4.Visible = true;
+            }
+
+            // Starting decoy
+            if (random.Next(0, 10) < 11)
+            {
+                int maxWidth = xMax - xMin - button4.Width;
+                int maxHeight = yMax - yMin - button4.Height;
+
+                button5.Left = random.Next(xMin, xMax - button4.Width);
+                button5.Top = random.Next(yMin, yMax - button4.Height);
+                button5.Visible = true;
+            }
+
+            // Decoy after score is higher than 10
+            if (random.Next(0, 10) < 11 && score > 10)
+            {
+                timer1.Interval = 900;
+                int maxWidth = xMax - xMin - button4.Width;
+                int maxHeight = yMax - yMin - button4.Height;
+
+                button6.Left = random.Next(xMin, xMax - button4.Width);
+                button6.Top = random.Next(yMin, yMax - button4.Height);
+                button6.Visible = true;
+            }
+
+            // Decoy after score is higher than 20
+            if (random.Next(0, 10) < 11 && score > 20)
+            {
+                timer1.Interval = 800;
+                int maxWidth = xMax - xMin - button4.Width;
+                int maxHeight = yMax - yMin - button4.Height;
+
+                button8.Left = random.Next(xMin, xMax - button4.Width);
+                button8.Top = random.Next(yMin, yMax - button4.Height);
+                button8.Visible = true;
+            }
+
+            // Bonus target
+            if (random.Next(0, 10) < 2)
+            {
+                int maxWidth = xMax - xMin - button4.Width;
+                int maxHeight = yMax - yMin - button4.Height;
+
+                button7.Left = random.Next(xMin, xMax - button4.Width);
+                button7.Top = random.Next(yMin, yMax - button4.Height);
+                button7.Visible = true;
+            }
+
+            if (score > 30)
+            {
+                timer1.Interval = 700;
+                
+            }
+
+            if (score > 40)
+            {
+                timer1.Interval = 600;
+            }
+
+            if (score > 50)
+            {
+                timer1.Interval = 500;
+            }
+
+            if (score > 60)
+            {
+                timer1.Interval = 400;
+            }
+
+            // Reset the stopwatch and additionalTime if elapsed is greater than or equal to 30 seconds
+            if (elapsed >= TimeSpan.FromSeconds(30))
+            {
+                timer1.Interval = 1000;
+                watch.Reset();
+                additionalTime = TimeSpan.FromSeconds(0);
+                timer1.Enabled = false;
+                button4.Visible = false;
+                button5.Visible = false;
+                button6.Visible = false;
+                button7.Visible = false;
+                button8.Visible = false;
+
+                if (score > highscore)
+                {
+                    highscore = score;
+                }
+
+                label3.Text = $"High Score: {highscore}";
+                score = 0;
+                label4.Visible = true;
+                return; // exit the Tick method early
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            button4.Visible = false;
+            score++;
+            label2.Text = $"Score: {score}";
+        }
+
+        private void Form1_Click(object sender, EventArgs e)
+        {
+            additionalTime += TimeSpan.FromSeconds(5);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            additionalTime += TimeSpan.FromSeconds(5);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            additionalTime += TimeSpan.FromSeconds(5);
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            additionalTime += TimeSpan.FromSeconds(-5);
+            button7.Visible = false;
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            additionalTime += TimeSpan.FromSeconds(5);
+        }
+    }
+}
